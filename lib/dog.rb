5b4b786d48f5fb3a022867bb88a0f41fd96e3ab1
trevor dewindt <trevor.dewindt@gmail.com>
2022-12-04 14:47:53 -0700

    commit tree 14fdb72d74e36c027a6512f769e54941adb9bccb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
-end
+    # setter method
+    def name=(value)
+        @this_dogs_name = value
+    end
+  
+    # getter method
+    def name
+      # access the data from the @name instance variable and return it
+      @this_dogs_name
+    end
+  end
+  
+  odie = Dog.new
+  odie.name = "Odie"
+  
+  # call the Dog#name method
+  puts odie.name
+  # "Odie"
